db11e8e7d949a0080b19e3a84634b7bbf51aa7e8
orlando-santos <santos.orlando.martin@gmail.com>
2024-11-30 19:58:31 +0000

    :recycle: Change class by interface

--- src/app/models/Product.ts
+++ src/app/models/Product.ts
@@ -7,13 +7,3 @@ export interface Product {
     discount: number | null;
     favorite: boolean;
   }
-
-// export class Product {
-//     id!: number;
-//     name!: string;
-//     description!: string;
-//     price!: number;
-//     imageUrl!: string;
-//     discount!: number;
-//     favorite!: boolean;
-//   }
