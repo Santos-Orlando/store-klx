71d1dd36ad74d5ccea602f05d2bb838fbb55f31f
orlando-santos <santos.orlando.martin@gmail.com>
2024-11-30 20:03:50 +0000

    :recycle: Create loadBase64Image method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
 import { CommonModule } from '@angular/common';
+import { Product } from './models/Product';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +11,28 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  products: any[] = [];
+  products: Product[] = []; 
+  base64Images: { [key: number]: string } = {};
 
   constructor(private apiService: ApiService) {}
 
-  
-
   ngOnInit() {
-      this.apiService.getProducts().subscribe((data) => {
-          this.products = data;
-      });
+    this.apiService.getProducts().subscribe((products: Product[]) => { 
+    this.products = products;
+    this.products.forEach(product => this.loadBase64Image(product));
+    });
   }
 
+  async loadBase64Image(product: Product) {
+    try {
+      const response = await fetch(product.imageUrl);
+      let base64String = await response.text();
+  
+      base64String = base64String.replace(/\s/g, '');
+  
+      this.base64Images[product.id] = `data:image/png;base64,${base64String}`;
+    } catch (error) {
+      console.error(`Error loading image for product ${product.id}:`, error);
+    }
+  }
 }
